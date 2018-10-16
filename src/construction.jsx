c9f9bca4ba7deae1b7af79390df40b7777e1cb6f
jcosentino <johnsc301@gmail.com>
2018-10-16 09:33:38 -0400

    10/16/2018 9:33am] - updated the <h> tags for construction.jsx

--- src/construction.jsx
+++ src/construction.jsx
@@ -6,14 +6,14 @@ class Construction extends React.Component {
 		return (
 			<div class='container' id='main'>
 				<h1>Page under construction!</h1>
-      			<h1>
+      			<h2>
         			See my portfolio on:
         			<a href='https://github.com/jcosentino'> Github - github.com/jcosentino</a>
-      			</h1>
+      			</h2>
 			
-			<h1>
+			<h3>
 				LinkedIn Profile: <a href='https://www.linkedin.com/in/john-cosentino/'>LinkedIn - John Cosentino</a>
-			</h1>
+			</h3>
     		</div>
 		);
 	}
