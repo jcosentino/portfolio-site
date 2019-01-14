136eb2e2ecd9f296911aa4764fe403a77fe24ff7
John Cosentino <johnsc301@gmail.com>
2019-01-13 21:08:43 -0500

    [01/13/19] - 9:08pm - fixed class / className inconsistencies

--- src/footer.jsx
+++ src/footer.jsx
@@ -5,33 +5,33 @@ class Footer extends Component {
 	render(){
 		return (
 			<div>
-				<footer class='footer text-center'>
+				<footer className='footer text-center'>
 					<a href='https://eff.org/'>
-						<img class='footer_img'
+						<img className='footer_img'
 							 src='../images/eff2019.png'
 							 alt='EFF'/>
 					</a>
 
 					<a href='https://reactjs.org/'>
-						<img class='footer_img'
+						<img className='footer_img'
 							 src='../images/react.png'
 							 alt='ReactJS'/>
 					</a>
 
 					<a href='https://www.linkedin.com/in/john-cosentino/'>
-						<img class='footer_img'
+						<img className='footer_img'
 							 src='../images/linkedin.png'
 							 alt='LinkedIn'/>
 					</a>
 
 					<a href='https://github.com/jcosentino'>
-						<img class='footer_img'
+						<img className='footer_img'
 							 src='../images/github.png'
 							 alt='Github:jcosentino/'/>
 					</a>
 
 					<a href='https://www.csi.cuny.edu/'>
-						<img class='footer_img'
+						<img className='footer_img'
 							 src='../images/csi.jpg'
 							 alt='CUNY - CSI'/>
 					</a>
--- src/title.jsx
+++ src/title.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Title extends React.Component {
 	render(){
 		return (
-			<div class='jumbotron text-center'>
+			<div className='jumbotron text-center'>
         		<h1>John Cosentino</h1>
         		<h3>Software Engineer</h3>
         	</div>
