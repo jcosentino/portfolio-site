27b3cd7e3a05b5c74520dc598ea539d22af26968
jcosentino <johnsc301@gmail.com>
2019-01-24 12:11:04 -0500

    [01/24/2019] - 12:10pm - added email link to Title, changed spacing of Title, added title.css

--- src/Title/title.jsx
+++ src/Title/title.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import './title.css';
 
 class Title extends React.Component {
 	render(){
 		return (
-			<div className='jumbotron text-center'>
+			<div id='title-area' className='jumbotron text-center'>
         		<h1>John Cosentino</h1>
         		<h3>Software Engineer</h3>
+				<p>Contact: <a href='john.s.cosentino@gmail.com'>john.s.cosentino@gmail.com</a></p>
         	</div>
 		);
 	}
