18d293ebda4d360b10ae91386c27c9f4c23a8180
jcosentino <johnsc301@gmail.com>
2019-01-24 12:14:10 -0500

    [01/24/2019] - 12:13pm - added mailto option to Title email link

--- src/Title/title.jsx
+++ src/Title/title.jsx
@@ -7,7 +7,7 @@ class Title extends React.Component {
 			<div id='title-area' className='jumbotron text-center'>
         		<h1>John Cosentino</h1>
         		<h3>Software Engineer</h3>
-				<p>Contact: <a href='john.s.cosentino@gmail.com'>john.s.cosentino@gmail.com</a></p>
+				<p>Contact: <a href='mailto:john.s.cosentino@gmail.com'>john.s.cosentino@gmail.com</a></p>
         	</div>
 		);
 	}
