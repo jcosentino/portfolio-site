c7c99d7693ca3fdf196a83a2e78fc2b6fe3edd1d
jcosentino <johnsc301@gmail.com>
2019-01-24 11:49:18 -0500

    [01/24/2019] - 11:48am - changed Footer css and jsx to created better spacing, and allowed clicking of Footer links to open in new tab

--- src/Footer/footer.jsx
+++ src/Footer/footer.jsx
@@ -5,37 +5,93 @@ class Footer extends Component {
 	render(){
 		return (
 			<div>
-				<footer className='footer text-center'>
-					<a href='https://eff.org/'>
+				<table id='footer-table'>
+					<tr>
+						<td>
+							<a href='https://eff.org/' target='_blank'>
+								<span><img className='footer_img'
+										   src='../images/eff2019.png'
+							 			   alt='EFF'/>
+							 	</span>
+							</a>
+						</td>
+
+						<td>
+							<a href='https://www.linkedin.com/in/john-cosentino/' target='_blank'>
+								<span><img className='footer_img'
+										   src='../images/linkedin.png'
+							 			   alt='LinkedIn'/>
+							 	</span>
+							</a>
+						</td>
+
+						<td>
+							<a href='https://reactjs.org/' target='_blank'>
+								<span><img className='footer_img'
+										   src='../images/react.png'
+							 			   alt='ReactJS'/>
+							 	</span>
+							</a>
+						</td>
+
+						<td>
+							<a href='https://github.com/jcosentino' target='_blank'>
+								<span><img className='footer_img'
+										   src='../images/github.png'
+							 			   alt='Github'/>
+							 	</span>
+							</a>
+						</td>
+
+						<td>
+							<a href='https://www.csi.cuny.edu/' target='_blank'>
+								<span><img className='footer_img'
+										   src='../images/csi.jpg'
+							 			   alt='CUNY - CSI'/>
+							 	</span>
+							</a>
+						</td>
+					</tr>
+				</table>
+				{/*<footer className='footer text-center'>
+					<a style={{display: "table-cell"}} 
+					   target="_blank" 
+					   href='https://eff.org/'>
 						<img className='footer_img'
 							 src='../images/eff2019.png'
 							 alt='EFF'/>
 					</a>
 
-					<a href='https://www.linkedin.com/in/john-cosentino/'>
+					<a style={{display: "table-cell"}} 
+					   target="_blank" 
+					   href='https://www.linkedin.com/in/john-cosentino/'>
 						<img className='footer_img'
 							 src='../images/linkedin.png'
 							 alt='LinkedIn'/>
 					</a>
 
-					<a href='https://reactjs.org/'>
+					<a style={{display: "table-cell"}} 
+					   target="_blank" 
+					   href='https://reactjs.org/'>
 						<img className='footer_img'
 							 src='../images/react.png'
 							 alt='ReactJS'/>
 					</a>
 
-					<a href='https://github.com/jcosentino'>
+					<a style={{display: "table-cell"}} target="_blank" href='https://github.com/jcosentino'>
 						<img className='footer_img'
 							 src='../images/github.png'
 							 alt='Github:jcosentino/'/>
 					</a>
 
-					<a href='https://www.csi.cuny.edu/'>
+					<a style={{display: "table-cell"}} 
+					   target="_blank" 
+					   href='https://www.csi.cuny.edu/'>
 						<img className='footer_img'
 							 src='../images/csi.jpg'
 							 alt='CUNY - CSI'/>
 					</a>
-				</footer>
+				</footer>*/}
 			</div>
 		);
 	}
