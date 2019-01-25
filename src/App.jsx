b9a6c5e6358e396633dfba1c574ae8aae5b58a17
jcosentino <johnsc301@gmail.com>
2019-01-24 22:31:15 -0500

    [01/24/2019] - 10:30pm - added more sidebar items, a temporary placeholder for unfinished features, and added a nice hover effect to the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ErrorPage from './Error_page/error-page';
 import Sidebar from './Sidebar/sidebar';
 import ResumeItems from './Resume/resume-items';
 import About from './About/about';
+import Placeholder from './Placeholder/placeholder';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
@@ -20,6 +21,16 @@ class App extends Component {
                 <Route path='/' exact component={Home} />
                 <Route path='/about' exact component={About} />
                 <Route path='/employment' exact component={ResumeItems} />
+                <Route path='/education' exact component={Placeholder} />
+                <Route path='/study-abroad' exact component={Placeholder} />
+                <Route path='/volunteer' exact component={Placeholder} />
+                <Route path='/portfolio-site' exact component={Placeholder} />
+                <Route path='/csc450-project' exact component={Placeholder} />
+                <Route path='/ctp-2016' exact component={Placeholder} />
+                <Route path='/testdriven-project' exact component={Placeholder} />
+                <Route path='/keras-docker-sample' exact component={Placeholder} />
+                <Route path='/csc430-project' exact component={Placeholder} />
+                <Route path='/csc305-lab2' exact component={Placeholder} />
                 <Route component={ErrorPage} />
               </Switch>
             </div>
--- src/Sidebar/sidebar.jsx
+++ src/Sidebar/sidebar.jsx
@@ -9,6 +9,16 @@ class Sidebar extends Component {
 				<a href='/'>Home</a>
 				<a href='/about'>About Me</a>
 				<a href='/employment'>Employment</a>
+				<a href='/education'>Education</a>
+				<a href='/study-abroad'>Study Abroad</a>
+				<a href='/volunteer'>Volunteerism</a>
+				<a href='/portfolio-site'>johnscosentino.com</a>
+				<a href='/csc450-project'>Research</a>
+				<a href='/ctp-2016'>CTP 2016</a>
+				<a href='/testdriven-project'>Testdriven</a>
+				<a href='/keras-docker-sample'>Keras / Docker Example</a>
+				<a href='/csc430-project'>CSC 430 Project (C#)</a>
+				<a href='/csc305-lab2'>Fit Algorithms (C++)</a>
 			</div>
 		);
 	}
