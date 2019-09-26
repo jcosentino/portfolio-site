f03bde5dc21426452f7a1996fe68eb64fac683b7
jcosentino <johnsc301@gmail.com>
2019-09-26 00:50:44 -0400

    [9/26/2019] - 12:50am - cleaned up the About section

--- src/components/Home/components/About/About.jsx
+++ src/components/Home/components/About/About.jsx
@@ -9,14 +9,10 @@ export class About extends React.Component {
         <div className='home-item'>
           <h1>About<span role='img' aria-label='waving hand'>👋</span></h1>
           <div className='about-item'>
-            <p>I am from Staten Island, NY, USA.</p>
-            <span className='separator-dot-about'>&#183;</span>
-            <p>In my free time I study data structures, play on my Switch, excercise, and practice guitar.</p>
-            <span className='separator-dot-about'>&#183;</span>
-            <p>My short-term goals include moving out on my own and landing a full-time position.</p>
-            <span className='separator-dot-about'>&#183;</span>
-            <p>My long-term goal is to make a major, widespread, and beneficial impact on this world.</p>
-            <span className='separator-dot-about'>&#183;</span>
+            <p>Born and raised in Staten Island, NY.</p>
+            <span role='img' aria-label='pointing down'>👇</span>
+            <p>Currently working in Newark, NJ.</p>
+            <span role='img' aria-label='looking eyes'>👀</span>
             <p>I want to be admired like Steve Jobs, impactful like Linus Torvalds, and honored like FDR.</p>
           </div>
         </div>
