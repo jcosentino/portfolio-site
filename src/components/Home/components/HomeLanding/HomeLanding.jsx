e220d974923808ad1a63209f120cf3de28ffd7ee
jcosentino <johnsc301@gmail.com>
2020-03-22 19:23:28 -0400

    [3/22/20] - 7:23pm - fixed broken image link

--- src/components/Home/components/HomeLanding/HomeLanding.jsx
+++ src/components/Home/components/HomeLanding/HomeLanding.jsx
@@ -7,7 +7,7 @@ export function HomeLanding() {
       <div className='home-img-section'>
         <h1>John Cosentino</h1>
         <div className='post-h1-break'></div>
-        <img src='me.jpg' alt='John C' />
+        <img src='self/me.jpg' alt='John C' />
         <p>^ circa 2017</p>
       </div>
     <span className='separator-dot'>&#183;</span>
