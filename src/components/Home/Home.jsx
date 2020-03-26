0fbcc75b6de511f8a199b6b945a3ba937d3a8e27
jcosentino <johnsc301@gmail.com>
2020-03-25 21:17:57 -0400

    [3/25/20] - 9:17pm - added fade-in animation to home items

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,18 +9,53 @@ import {
   Events,
   About,
    } from './components';
+   
+function FadeInSection(props) {
+  const [isVisible, setVisible] = React.useState(false);
+  const homeRef = React.useRef();
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(divs => {
+      divs.forEach(div => setVisible(div.isIntersecting));
+    });
+    observer.observe(homeRef.current);
+  }, []);
+  return (
+    <div className={`home-fade-in ${isVisible ? 'visible' : ''}`}
+         ref={homeRef}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+function createHomeItem(div){
+  return (
+    <FadeInSection>
+      {div}
+    </FadeInSection>
+  );
+}
+
+const HOME_TEMS = [
+  <HomeLanding />,
+  <Experience />,
+  <Education />,
+  <Projects />,
+  <Software />,
+  <Events />,
+  <About />
+];
+
+const HOME_ITEM_DIVS = HOME_TEMS.map(div => 
+  createHomeItem(div)
+);
 
 export function Home(){
   return (
     <div className='Home'>
-      <HomeLanding />
+      {HOME_ITEM_DIVS[0]}
       <div className='home-menu-items'>
-        <Experience />
-        <Education />
-        <Projects />
-        <Software />
-        <Events />
-        <About />
+        {HOME_ITEM_DIVS.slice(1)}
       </div>
     </div>
   );
